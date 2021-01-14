0d3cbdc73d4d7f6d5f8dfd95b181696040e2383d
Santosh <santosh.swarnim@gmail.com>
2021-01-14 18:22:59 +0545

    changes in ui and back end

---
+++ WebApplication12/AuthorizeAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication12
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    namespace webApiTokenAuthentication
+    {
+        public class AuthorizeAttribute : System.Web.Http.AuthorizeAttribute
+        {
+            protected override void HandleUnauthorizedRequest(System.Web.Http.Controllers.HttpActionContext actionContext)
+            {
+                if (!HttpContext.Current.User.Identity.IsAuthenticated)
+                {
+                    base.HandleUnauthorizedRequest(actionContext);
+                }
+                else
+                {
+                    actionContext.Response = new System.Net.Http.HttpResponseMessage(System.Net.HttpStatusCode.Forbidden);
+                }
+            }
+        }
+    }
+}

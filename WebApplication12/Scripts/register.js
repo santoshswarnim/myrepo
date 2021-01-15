fa9ad288702ae1bfeb0dc5ac86e5d59fbef78707
Santosh <santosh.swarnim@gmail.com>
2021-01-15 11:45:06 +0545

    validation

---
+++ WebApplication12/Scripts/register.js
@@ -0,0 +1,22 @@
+﻿$(function (){
+    var $registerForm = $("#form_id");
+    if ($registerForm.length) {
+        $registerForm.validate()
+        $register.Form.validate({
+            rules: {
+                name: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    reuired: 'name is mandatory!'
+                }
+            }
+           
+
+        })
+    }
+    
+
+})
